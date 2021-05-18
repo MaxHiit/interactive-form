2305b498eb27c926d1769b79100c4e773a955eb6
MaxHiit <pro.maxcode@gmail.com>
2021-05-19 00:20:03 +0200

    feat(card): card for the home page

---
+++ src/components/card/CardInfo.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import NextButton from '../next-button/NextButton';
+import './cardInfo.scss';
+
+const CardInfo = ({ data }) => {
+	return (
+		<li className='card'>
+			<div className='card--img-wrapper'>
+				<img src={data.src} alt='gilbert-placard' />
+			</div>
+			<h2 className='card--name'>{data.name}</h2>
+			<p className='card--desc'>{data.description}</p>
+			<p className='card--detail'>
+				Espace de stockage: {data.stockageSpaceDetail} <br /> ({data.m3Storage}m3)
+			</p>
+			<NextButton optionsData={data} />
+			<h3 className='card--price'>{data.price}€/mois</h3>
+		</li>
+	);
+};
+
+export default CardInfo;
