99a767afd18ce0f627cca65a7d804570458d2454
MaxHiit <pro.maxcode@gmail.com>
2021-05-19 00:16:10 +0200

    feat(home page): structure, data and style of the home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Home from './components/home/Home';
+import Home from './pages/home/Home';
 
 export const App = () => {
 	return (
