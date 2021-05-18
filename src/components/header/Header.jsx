2d6ecb346fd44b9765ab98640a1ee7cedfbbcd4f
MaxHiit <pro.maxcode@gmail.com>
2021-05-18 23:23:20 +0200

    feat(page title): initial title component for all page

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+	return (
+		<header id='header'>
+			<h1>De combien d'espace avez-vous besoin ?</h1>
+		</header>
+	);
+};
+
+export default Header;
