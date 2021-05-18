8421f8f999484b684edc46bd83c617b651e523fa
MaxHiit <pro.maxcode@gmail.com>
2021-05-18 23:22:15 +0200

    feat(route | reset css | package): adding reset css, route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/home/Home';
 
 export const App = () => {
-	return <h1>You know me ?</h1>;
+	const [step, setStep] = useState([
+		{ name: 'step 1', options: {}, isValidate: false },
+		{ name: 'step 2', options: {}, isValidate: false },
+		{ name: 'step 3', options: {}, isValidate: false }
+	]);
+
+	return (
+		<>
+			<Switch>
+				<Route path='/'>
+					<Home setStep={setStep} step={step} />
+				</Route>
+			</Switch>
+		</>
+	);
 };
 
 export default App;
