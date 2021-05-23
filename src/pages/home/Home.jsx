7ba790c5c3c32229e670d5ac8a306abb943635f0
MaxHiit <pro.maxcode@gmail.com>
2021-05-23 05:56:37 +0200

    refactor(data structure):

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,49 +1,20 @@
 import React from 'react';
 import Header from '../../components/header/Header';
-import CardInfo from '../../components/card/CardInfo';
+import Card from '../../components/card/Card';
+import { getHomeData } from '../../data.js';
 import './home.scss';
-import srcGilbert from '../../images/gimbert-color.png';
 
 const Home = () => {
-	const getSpaceData = [
-		{
-			id: 0,
-			name: 'Placard',
-			description: `Idéal si vous n'avez que quelques affaires à stocker. Parfait pour quelques cartons ou valises.`,
-			src: srcGilbert,
-			stockageSpaceDetail: '0.5m x 1m x 1m',
-			m3Storage: '0.5',
-			price: 24
-		},
-		{
-			id: 1,
-			name: 'Cave',
-			description: `Idéal pour une quinzaine de cartons ou des petits meubles. Parfait pour libérer une pièces !`,
-			src: srcGilbert,
-			stockageSpaceDetail: '1.5m x 1m x 1m',
-			m3Storage: '1.5',
-			price: 36
-		},
-		{
-			id: 2,
-			name: 'Placard',
-			description: `Convient pour des meubles de taille moyenne telles qu'une table, des chaises, un, réfrigérateur, etc.`,
-			src: srcGilbert,
-			stockageSpaceDetail: '3m x 1m x 1m',
-			m3Storage: '3',
-			price: 54
-		}
-	];
 	return (
 		<>
 			<Header />
 			<main id='home-container'>
 				<ul className='space-container'>
-					{getSpaceData.map((spaceData) => {
-						return <CardInfo key={spaceData.name + spaceData.id} data={spaceData} />;
+					{getHomeData.map((spaceData) => {
+						return <Card key={spaceData.name + spaceData.id} data={spaceData} />;
 					})}
 				</ul>
-				<a className='space-link' href=''>
+				<a className='space-link' href='/'>
 					Aidez-moi à estimer mon volume de stockage
 				</a>
 			</main>
--- src/pages/spaceRecap/SpaceRecap.jsx
+++ src/pages/spaceRecap/SpaceRecap.jsx
@@ -1,4 +1,4 @@
-import React, { isValidElement, useContext } from 'react';
+import React, { useContext } from 'react';
 import { Context } from '../../Context';
 import NextButton from '../../components/next-button/NextButton';
 import srcSpaceRecap from '../../images/space-recap.png';
@@ -8,8 +8,8 @@ import MotionSensor from '../../components/icon/MotionSensor';
 import Fire from '../../components/icon/Fire';
 import Sun from '../../components/icon/Sun';
 import Check from '../../components/icon/Check';
+import { stepIndexes } from '../../data';
 import './space-recap.scss';
-
 const SpaceRecap = () => {
 	const { step } = useContext(Context);
 	return (
@@ -108,7 +108,7 @@ const SpaceRecap = () => {
 					</div>
 				</div>
 			</div>
-			<NextButton srcNextStep='/help' />
+			<NextButton srcNextStep='/help' stepIndex={stepIndexes.space_recap} />
 		</div>
 	);
 };
